58291d04a2653bd2c2bcf8c4ecdf63453ba7ff75
IvanRubioSabate <ivan.rubio@institutvidreres.cat>
2025-01-23 10:51:13 +0100

    Toolbar created

--- app/src/main/java/com/novelight/application/MainActivity.kt
+++ app/src/main/java/com/novelight/application/MainActivity.kt
@@ -3,18 +3,32 @@ package com.novelight.application
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var toolbar: Toolbar
+    private lateinit var appBarConfig: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val navHostFragment: NavHostFragment = supportFragmentManager.findFragmentById(R.id.mainFragmentView) as NavHostFragment
+        navController = navHostFragment.navController
+
+        appBarConfig = AppBarConfiguration(navController.graph)
+
+        findViewById<Toolbar>(R.id.mainToolbar)
+            .setupWithNavController(navController, appBarConfig)
     }
 }
