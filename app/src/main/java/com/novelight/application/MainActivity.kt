fef9daa99b9ab93600a8f88c1addfff7035bd96f
IvanRubioSabate <ivan.rubio@institutvidreres.cat>
2025-01-23 11:13:55 +0100

    Activity config created

--- app/src/main/java/com/novelight/application/MainActivity.kt
+++ app/src/main/java/com/novelight/application/MainActivity.kt
@@ -1,27 +1,30 @@
 package com.novelight.application
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.drawerlayout.widget.DrawerLayout
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import com.novelight.application.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
-    private lateinit var drawerLayout: DrawerLayout
-    private lateinit var toolbar: Toolbar
     private lateinit var appBarConfig: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
 
         val navHostFragment: NavHostFragment = supportFragmentManager.findFragmentById(R.id.mainFragmentView) as NavHostFragment
         navController = navHostFragment.navController
@@ -30,5 +33,10 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<Toolbar>(R.id.mainToolbar)
             .setupWithNavController(navController, appBarConfig)
+
+        binding.settingsButton.setOnClickListener {
+            val intent = Intent(this, ConfigActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
