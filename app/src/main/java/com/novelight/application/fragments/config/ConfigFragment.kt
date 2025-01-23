693d103efa014f102428fc9d49de38c02c5b39f2
Michelsaez610 <michel.saez@institutvidreres.cat>
2025-01-23 09:50:32 +0100

    All fragments created

--- app/src/main/java/com/novelight/application/fragments/config/ConfigFragment.kt
+++ app/src/main/java/com/novelight/application/fragments/config/ConfigFragment.kt
@@ -1,4 +1,4 @@
-package com.novelight.application.fragments
+package com.novelight.application.fragments.config
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [LibraryFragment.newInstance] factory method to
+ * Use the [ConfigFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class LibraryFragment : Fragment() {
+class ConfigFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class LibraryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_library, container, false)
+        return inflater.inflate(R.layout.fragment_config, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class LibraryFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment LibraryFragment.
+         * @return A new instance of fragment ConfigFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            LibraryFragment().apply {
+            ConfigFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
