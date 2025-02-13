280ab20fca85868907affda05c4bf4aa01008ed5
IvanRubioSabate <ivan.rubio@institutvidreres.cat>
2025-02-13 08:41:13 +0100

    made toolbar menu easier to work with and scale

--- app/src/main/java/com/novelight/application/models/MaterialToolbarDestinationManager.kt
+++ app/src/main/java/com/novelight/application/models/MaterialToolbarDestinationManager.kt
@@ -1,6 +1,5 @@
 package com.novelight.application.models
 
-import android.hardware.SensorPrivacyManager
 import android.widget.ImageView
 import android.widget.SearchView
 import androidx.core.view.forEach
@@ -18,40 +17,17 @@ class MaterialToolbarDestinationManager(
     private val filterViewModel: FilterViewModel
 ) {
 
-    private val homeFragments: Set<Int> = setOf(
-        R.id.libraryFragment,
-        R.id.updatesFragment,
-        R.id.historyFragment,
-        R.id.exploreFragment
-    )
+    init {
+        setToolBarMenuOptions()
+    }
 
     fun onFragmentChange(destination: NavDestination) {
         closeSearchViewIfExpanded()
-
-        when (destination.id) {
-            R.id.libraryFragment -> {
-                fragmentLibraryMenuOptions()
-            }
-
-            R.id.updatesFragment -> {
-                fragmentUpdatesMenuOptions()
-            }
-
-            R.id.historyFragment -> {
-                fragmentHistorialMenuOptions()
-            }
-
-            R.id.exploreFragment -> {
-                fragmentExplorarMenuOptions()
-            }
-        }
-
-        materialToolbar.menu.findItem(R.id.settings).isVisible =
-            homeFragments.contains(destination.id)
+        showToolBarGroup(destination.id)
     }
 
     private fun goToSettingsFragment(actionId: Int) {
-        materialToolbar.menu.forEach { it.isVisible = false }
+        showToolBarGroup(null)
         navController.navigate(actionId)
     }
 
@@ -62,76 +38,44 @@ class MaterialToolbarDestinationManager(
         }
     }
 
-    private fun fragmentLibraryMenuOptions() {
-        showToolBarGroup(R.id.libraryGroup)
-        setupToolBarSearchView()
+    private fun setToolBarMenuOptions() {
+        setupLibraryToolBarSearchView()
 
         materialToolbar.setOnMenuItemClickListener { menuItem ->
             when (menuItem.itemId) {
-                R.id.librarySearch -> {
-                    setupToolBarSearchView()
-                    true
-                }
-
-                R.id.settings -> {
+                // libraryMenu
+                R.id.librarySettings -> {
                     goToSettingsFragment(R.id.action_libraryFragment_to_configFragment3)
                     true
                 }
 
-                else -> false
-            }
-        }
-    }
-
-    private fun fragmentUpdatesMenuOptions() {
-        showToolBarGroup(R.id.updatesGroup)
-
-        materialToolbar.setOnMenuItemClickListener { menuItem ->
-            when (menuItem.itemId) {
+                // updatesMenu
                 R.id.updatesTest -> {
                     goToSettingsFragment(R.id.action_updatesFragment_to_configFragment3)
                     true
                 }
 
-                R.id.settings -> {
+                R.id.updatesSettings -> {
                     goToSettingsFragment(R.id.action_updatesFragment_to_configFragment3)
                     true
                 }
 
-                else -> false
-            }
-        }
-    }
-
-    private fun fragmentHistorialMenuOptions() {
-        showToolBarGroup(R.id.historyGroup)
-
-        materialToolbar.setOnMenuItemClickListener { menuItem ->
-            when (menuItem.itemId) {
+                // historyMenu
                 R.id.historyTest -> {
                     true
                 }
 
-                R.id.settings -> {
+                R.id.historySettings -> {
                     goToSettingsFragment(R.id.action_historyFragment_to_configFragment3)
                     true
                 }
 
-                else -> false
-            }
-        }
-    }
-
-    private fun fragmentExplorarMenuOptions() {
-        showToolBarGroup(R.id.exploreGroup)
-
-        materialToolbar.setOnMenuItemClickListener { menuItem ->
-            when (menuItem.itemId) {
+                // exploreMenu
                 R.id.exploreTest -> {
                     true
                 }
 
-                R.id.settings -> {
+                R.id.exploreSettings -> {
                     goToSettingsFragment(R.id.action_exploreFragment_to_configFragment3)
                     true
                 }
@@ -141,7 +85,7 @@ class MaterialToolbarDestinationManager(
         }
     }
 
-    private fun setupToolBarSearchView() {
+    private fun setupLibraryToolBarSearchView() {
         val searchMenuItem = materialToolbar.menu.findItem(R.id.librarySearch)
         val searchView = searchMenuItem.actionView as SearchView
         searchView.setOnQueryTextListener(MyOnQueryTextListener(filterViewModel))
@@ -155,7 +99,12 @@ class MaterialToolbarDestinationManager(
         searchMenuItem.setOnActionExpandListener(MyOnActionExpandListener(searchView))
     }
 
-    private fun showToolBarGroup(id: Int) {
-        materialToolbar.menu.forEach { it.isVisible = it.groupId == id }
+    private fun showToolBarGroup(id: Int?) {
+        if (id != null) {
+            materialToolbar.menu.forEach { it.isVisible = it.groupId == id }
+        }
+        else {
+            materialToolbar.menu.forEach { it.isVisible = false }
+        }
     }
 }
